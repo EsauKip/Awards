2255084071890e9bfac6ba3b5a1bde3dfe613b9a
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-08 15:05:53 +0300

    create class

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class profile(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    profile_pic=models.ImageField(upload_to='profile_pics',blank=True)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+   
