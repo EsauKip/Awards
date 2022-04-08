6a9df1966161fdddd3fcaf5eac757f2e255158eb
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-08 15:06:18 +0300

    add save profile method

--- projects/models.py
+++ projects/models.py
@@ -1,15 +1,22 @@
-from django.db import models
 
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 # Create your models here.
 class profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=100)
-    profile_pic=models.ImageField(upload_to='profile_pics',blank=True)
+    profile_pic = CloudinaryField('images')
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zipcode = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    user = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    def save_profile(self):
+        self.save()
+            
    
