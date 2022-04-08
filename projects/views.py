7913d8e81be5ffcd35116d0c74b0aa23cd772e58
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-08 23:06:37 +0300

    add imports

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,29 @@
-import profile
-from django.shortcuts import render
-from .models import Project,Profile
+from django.http.response import HttpResponse, HttpResponseRedirect
+from .models import Profile, Project, Rates
+from django.contrib.auth.models import User
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
 # Create your views here.
 def index(request):
     profile = Profile.objects.all()
     projects = Project.objects.all()
     return render(request,'index.html',{"projects":projects,"profile":profile})
+
+
+#search view function
+@login_required(login_url='/accounts/login/')
+def search(request):
+    if 'project' in request.GET and request.GET['project']:
+        project = request.GET.get("project")
+        results = Project.search_project(project)
+        message = f'project'
+        return render(request, 'search.html', {'projects': results, 'message': message})
+    else:
+        message = "You haven't searched for anything, please try again"
+    return render(request, 'search.html', {'message': message})
+
+    
