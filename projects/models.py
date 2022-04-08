c1915b44d29579001ead9343741502921c60103c
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-08 15:19:03 +0300

    add projects class

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,5 @@
 
+from os import name
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
@@ -34,6 +35,31 @@ class profile(models.Model):
     def search_profile(cls,search_term):
         profile = cls.objects.filter(name__icontains=search_term)
         return profile
-        
 
-   
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    url=models.URLField(max_length=100)
+    description = models.TextField()
+    image = CloudinaryField('images')
+    link = models.CharField(max_length=100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile = models.ForeignKey(profile,on_delete=models.CASCADE)
+    date=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+    def save_project(self):
+        self.save()
+    def delete_project(self):
+        self.delete()
+    def update_project(self):
+        self.update()
+    @classmethod
+    def get_project(cls,id):
+        project = cls.objects.get(id=id)
+        return project
+    @classmethod
+    def search_project(cls,search_term):
+        project = cls.objects.filter(title__icontains=name).all()
+        return project
+
