faa08d9d7c567860c3061f259492649cb565cc2e
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-09 22:44:44 +0300

    create update user form function

--- projects/forms.py
+++ projects/forms.py
@@ -7,4 +7,14 @@ class SignUpForm(UserCreationForm):
     full_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
     class Meta:
+        model = User
         fields=('username','full_name','email','password1','password2',)
+
+# updateUserForm
+class UpdateUserForm(forms.ModelForm):
+    email=forms.EmailField(max_length=30, required=False, help_text='Optional.')
+    class Meta:
+        model = User
+        fields=('username','email',)
+        
+
