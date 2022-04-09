a83bad4701048ff7d53a94d5bfa6a390382332f0
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-09 22:20:45 +0300

    create a class user serializer

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,3 +1,12 @@
+from rest_framework import serializers
+from .models import Profile,Project
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=('id','username','email','password')
+        
 
 
      
