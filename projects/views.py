5252248c067432a7293c333debf78faf2cc5ea4d
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-09 22:13:43 +0300

    create a profile list class

--- projects/views.py
+++ projects/views.py
@@ -7,6 +7,15 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
+
+
+#APIS
+from .models import Profile, Project, Rates
+from .serializers import ProfileSerializer, ProjectSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 def index(request):
     profile = Profile.objects.all()
@@ -103,3 +112,15 @@ def view_project(request, id):
 
     }
     return render(request, 'view-project.html', params)
+
+class ProfileList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+
+    def get(self, request, format=None):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
