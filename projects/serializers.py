ba7d1665713df93cdc63c32e482920372190f2c6
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-09 22:26:30 +0300

    create profile serializer

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,3 +1,4 @@
+from dataclasses import field
 from rest_framework import serializers
 from .models import Profile,Project
 from django.contrib.auth.models import User
@@ -6,7 +7,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=('id','username','email','password')
-        
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user=UserSerializer(many=False,read_only=True)
+    class Meta:
+        fields=['id','user', 'profile_pic', 'bio', 'contact']
+                
+
 
 
      
